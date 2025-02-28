93a1adb5703101688d79050ea589a1603c657bec
Boghost-alt <maxoumaxou1430@gmail.com>
2025-02-28 23:19:05 +0100

    Updated main.c
    Added support to read instructions from files

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ typedef struct{
     const char *reg;
     int value;
     Inst_set type;
-}Inst;
+} Inst;
 
 Inst parse_inst(char *line);
 
@@ -51,7 +51,7 @@ Inst instructions[] = {
 
 void skipspace(const char* str, size_t *currentPos)
 {
-    while(str[*currentPos]==' ')
+    while(str[*currentPos] == ' ')
     {
         (*currentPos)++;
     }
@@ -59,31 +59,31 @@ void skipspace(const char* str, size_t *currentPos)
 
 void push(int val, const char* reg)
 {
-    if(strcmp(reg, "stack") && stack_size < MAX_STACK_SIZE)
+    if(strcmp(reg, "stack") == 0 && stack_size < MAX_STACK_SIZE)
         stack[stack_size++] = val;
-    else if(strcmp(reg, "ax") && a_size < MAX_REG_SIZE)
+    else if(strcmp(reg, "ax") == 0 && a_size < MAX_REG_SIZE)
         ax[a_size++] = val;
-    else if(strcmp(reg, "bx") && b_size < MAX_REG_SIZE)
+    else if(strcmp(reg, "bx") == 0 && b_size < MAX_REG_SIZE)
         bx[b_size++] = val;
-    else if(strcmp(reg, "cx")&& c_size < MAX_REG_SIZE)
+    else if(strcmp(reg, "cx") == 0 && c_size < MAX_REG_SIZE)
         cx[c_size++] = val;
     else
-        printf("ERROR : Stack overflow");
+        printf("ERROR: Stack overflow\n");
 }
 
 int pop(const char* reg)
 {
-    if(strcmp(reg, "stack") && stack_size > 0)
+    if(strcmp(reg, "stack") == 0 && stack_size > 0)
         return stack[--stack_size];
-    else if(strcmp(reg, "ax") && a_size > 0)
+    else if(strcmp(reg, "ax") == 0 && a_size > 0)
         return ax[--a_size];
-    else if(strcmp(reg, "bx") && b_size > 0)
+    else if(strcmp(reg, "bx") == 0 && b_size > 0)
         return bx[--b_size];
-    else if(strcmp(reg, "cx")&& c_size > 0)
+    else if(strcmp(reg, "cx") == 0 && c_size > 0)
         return cx[--c_size];
     else
     {
-        printf("ERROR : Stack underflow");
+        printf("ERROR: Stack underflow\n");
         return 0;
     }
 }
@@ -122,56 +122,120 @@ void mov(int val, const char* reg)
 
 void print_register(char* reg)
 {
-    if(strcmp(reg, "ax")==0)
+    if(strcmp(reg, "ax") == 0)
     {
-        printf("ax : \n");
+        printf("ax: \n");
         for(int i = a_size - 1; i >= 0; i--)  
             printf("%d\n", ax[i]);  
-    }else if(strcmp(reg, "bx")==0)
+    }else if(strcmp(reg, "bx") == 0)
     {
-        printf("bx : \n");
+        printf("bx: \n");
         for(int i = b_size - 1; i >= 0; i--)  
             printf("%d\n", bx[i]);  
-    }else if(strcmp(reg, "cx")==0)
+    }else if(strcmp(reg, "cx") == 0)
     {
-        printf("cx :  \n");
+        printf("cx:  \n");
         for(int i = c_size - 1; i >= 0; i--)  
             printf("%d\n", cx[i]);  
-    }else if(strcmp(reg, "stack")==0)
+    }else if(strcmp(reg, "stack") == 0)
     {
-        printf("stack :  \n");
+        printf("stack:  \n");
         for(int i = stack_size - 1; i >= 0; i--)  
             printf("%d\n", stack[i]);  
     }
+}
 
+Inst parse_inst(char *line)
+{
+    Inst inst;
+    char buff[BUFSIZ];
+    size_t i = 0;
+
+    skipspace(line, &i);
+
+    // Get the instruction type
+    size_t j = 0;
+    while(line[i] != ' ' && line[i] != '\0') {
+        buff[j++] = line[i++];
+    }
+    buff[j] = '\0';
+
+    if (strcmp(buff, "PUSH") == 0)
+        inst.type = Inst_PUSH;
+    else if (strcmp(buff, "POP") == 0)
+        inst.type = Inst_POP;
+    else if (strcmp(buff, "ADD") == 0)
+        inst.type = Inst_ADD;
+    else if (strcmp(buff, "SUB") == 0)
+        inst.type = Inst_SUB;
+    else if (strcmp(buff, "MOV") == 0)
+        inst.type = Inst_MOV;
+    else {
+        printf("ERROR: Unknown instruction %s\n", buff);
+        exit(1);
+    }
+
+    skipspace(line, &i); // Skip space after instruction
+
+    j = 0;
+    while(line[i] != ' ' && line[i] != '\0') {
+        buff[j++] = line[i++];
+    }
+    buff[j] = '\0';
+
+    if(inst.type == Inst_PUSH || inst.type == Inst_MOV)
+        inst.value = atoi(buff);
+    else
+        inst.value = 0;
+
+    skipspace(line, &i); // Skip space after value
+
+    j = 0;
+    while(line[i] != '\0') {
+        buff[j++] = line[i++];
+    }
+    buff[j] = '\0';
+
+    if (strcmp(buff, "") == 0)
+        inst.reg = "stack";
+    else if (strcmp(buff, "ax") == 0)
+        inst.reg = "ax";
+    else if (strcmp(buff, "bx") == 0)
+        inst.reg = "bx";
+    else if (strcmp(buff, "cx") == 0)
+        inst.reg = "cx";
+    else {
+        printf("ERROR: Unknown register %s\n", buff);
+        exit(1);
+    }
+
+    return inst;
 }
 
 int main(int argc, char** argv)
 {
     if(argc < 2)
     {
-        printf("Incorrect usage, expected : ./main \"file.txt\"");
+        printf("Incorrect usage, expected: ./main file.txt\n");
         exit(1);
     }
     
     FILE *fp = fopen(argv[1], "r");
-    if(fp==NULL)
+    if(fp == NULL)
     {
-        fprintf(stderr, "ERROR : Unable to open file");
+        fprintf(stderr, "ERROR: Unable to open file\n");
         exit(1);
     }
-    stack_size = 0; // Explicit initialization
-
-    int a, b;
-    char buff[BUFSIZ];
     
+    char buff[BUFSIZ];
     while(fgets(buff, BUFSIZ, fp) != NULL)
     {
         size_t len = strlen(buff);
-        if(buff[len-1] == '\n')
-            buff[len-1] = '\0';
+        if(buff[len - 1] == '\n')
+            buff[len - 1] = '\0';
         
         Inst inst = parse_inst(buff);
+        int a, b;
         switch(inst.type)
         {
             case Inst_PUSH:
@@ -188,12 +252,11 @@ int main(int argc, char** argv)
             case Inst_SUB:
                 a = pop(inst.reg);
                 b = pop(inst.reg);   
-                push(a-b,inst.reg);
+                push(a - b, inst.reg);
                 break;
             case Inst_MOV:
                 mov(inst.value, inst.reg);
                 break;
-                
         }
     }
 
@@ -206,58 +269,3 @@ int main(int argc, char** argv)
     
     return 0;
 }
-
-Inst parse_int(char *line)
-{
-    Inst inst;
-    char buff[BUFSIZ];
-    size_t i;
-    
-    skipspace(line, &i);
-    
-    size_t j=0;
-    while(line[i] != ' ' && line[i] != '\0')
-        buff[j++] = line[i++];
-    buff[j] = '\0';
-    
-
-    if(strcmp(buff, "PUSH"))
-        inst.type = Inst_PUSH;
-    else if(strcmp(buff, "POP"))
-        inst.type = Inst_POP;
-    else if(strcmp(buff, "ADD"))
-        inst.type = Inst_ADD;
-    else if(strcmp(buff, "SUB"))
-        inst.type = Inst_SUB;
-    else if(strcmp(buff, "MOV"))
-        inst.type = Inst_MOV;
-
-    skipspace(line, &i);
-    j=0;
-    while(line[i] != '\0')
-        buff[j++] = line[i++];
-    buff[j] = '\0';
-    
-    if(inst.type == Inst_PUSH)
-        inst.value = atoi(buff);
-    else
-        inst.value = 0;
-    
-    skipspace(line, &i);
-    j=0;
-    while(line[i] != '\0')
-        buff[j++] = line[i++];
-    buff[j] = '\0';
-    
-    if(strcmp(buff, ""))
-        inst.reg = "stack";
-    else if(strcmp(buff, "ax"))
-        inst.reg = "ax";
-    else if(strcmp(buff, "bx"))
-        inst.reg = "bx";
-    else if(strcmp(buff, "cx"))
-        inst.reg = "cx";
-    
-    return inst;
-}
-
